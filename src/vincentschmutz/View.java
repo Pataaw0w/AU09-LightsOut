1c69014e424a0caea2a9a7cbd6d4ab816b29dfb7
Vincent <vincentschwartz7@gmail.com>
2016-11-22 16:13:03 +0100

    Update View.java

--- src/vincentschmutz/View.java
+++ src/vincentschmutz/View.java
@@ -1,12 +1,26 @@
 package vincentschmutz;
-
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.util.*;
+import javax.swing.*;
 /**
- * 
- *
+ * @author Vincent Schwartz
+ * @version 2016-11-21
  */
-
-public class View {
+public class View extends JFrame{
+	private Panel p;
+	private Controller c;
+	private LightsOut lo;
+	public View(Panel p, LightsOut lo, Controller c){
+		this.p = p;
+		this.c = c;
+		this.lo = lo;
+		this.setTitle("LightsOut");
+		this.setSize(1000, 1000);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(this.p);
+		this.setVisible(true);
+	}
 	
-	
-
 }
