423aaeef8476436a3cb5f46e0e7fe4ab7065c595
Vincent <vincentschwartz7@gmail.com>
2016-11-22 18:29:30 +0100

    Update Controller.java
    actionPerformed added

--- src/vincentschmutz/Controller.java
+++ src/vincentschmutz/Controller.java
@@ -1,34 +1,27 @@
 package vincentschmutz;
-
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- * @author Patrick Zhanel
+ * @author Patrick Zhanel, Vincent Schwartz
  * @version 2016-11-22
- * 
- * Controller-Klasse zur Verarbeitung der Aktionen der gedrückten Buttons
  *
  */
-public class Controller implements ActionListener {
-	
-	private LightsOut lo;
+public class Controller implements ActionListener{
 	private View v;
 	private Panel p;
-
+	private LightsOut lo;
 	public Controller(){
-		
-		this.lo = new LightsOut();
 		this.p = new Panel();
+		this.lo = new LightsOut();
 		this.v = new View(this.p, this.lo, this);
-		
-	}
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		
-		
-		
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(this.p.isJButton1((JButton)e.getSource())==true){}
+	}
+	public static void main(String[] args){
+		new Controller();
+	}
 }
