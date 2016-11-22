86866d7cb54b6b4b50ed458e48096f1478484b45
Pataaw0w <lolboy02@gmail.com>
2016-11-22 20:53:54 +0100

    Update LightsOut.java
    Hopefully finished

--- src/vincentschmutz/LightsOut.java
+++ src/vincentschmutz/LightsOut.java
@@ -1,17 +1,106 @@
 package vincentschmutz;
 
+import javax.swing.JButton;
+
 /**
  * @author Patrick Zhanel
  * @version 2016-11-19
  * 
- * Model-Klasse für die benötigten Methoden.
+ * Model-Klasse fÃ¼r die benÃ¶tigten Methoden.
+ * true - an, false - off
  */
 
 public class LightsOut {
 	
+	private boolean[][] lights;
+	
 	public LightsOut(){
 		
+		this.lights = new boolean[5][5];
 		
+		for(int i = 0; i < 5; i++){
+			
+			for(int j = 0; j < 5; j++){
+			
+			boolean random = Math.random() >= 0.5;
+			this.lights[i][j] = random;
+					
+			}
+			
+		}
+		
+	}
+	
+	/**
+	 * Method to see which button is pressed
+	 * 
+	 * @param x coordinate
+	 * @param y coordinate
+	 */
+	
+	public void changeColor(int x, int y){
+		
+		if(x != 0 && x != 5 && y != 0 && y != 5) {
+		
+			this.lights[x][y] = !this.lights[x][y];
+			this.lights[x+1][y] = !this.lights[x+1][y];
+			this.lights[x-1][y] = !this.lights[x-1][y];
+			this.lights[x][y+1] = !this.lights[x][y+1];
+			this.lights[x][y-1] = !this.lights[x][y-1];
+			
+		} else if(x == 0 && y != 0 && y != 5){
+			
+			this.lights[x][y] = !this.lights[x][y];
+			this.lights[x+1][y] = !this.lights[x+1][y];
+			this.lights[x][y+1] = !this.lights[x][y+1];
+			this.lights[x][y-1] = !this.lights[x][y-1];
+			
+		} else if(x == 5 && y != 0 && y != 5){
+			
+			this.lights[x][y] = !this.lights[x][y];
+			this.lights[x-1][y] = !this.lights[x-1][y];
+			this.lights[x][y+1] = !this.lights[x][y+1];
+			this.lights[x][y-1] = !this.lights[x][y-1];
+			
+		} else if(x != 0 && x != 5 && y == 0){
+			
+			this.lights[x][y] = !this.lights[x][y];
+			this.lights[x+1][y] = !this.lights[x+1][y];
+			this.lights[x-1][y] = !this.lights[x-1][y];
+			this.lights[x][y+1] = !this.lights[x][y+1];
+			
+		} else if(x != 0 && x != 5 && y == 5){
+			
+			this.lights[x][y] = !this.lights[x][y];
+			this.lights[x+1][y] = !this.lights[x+1][y];
+			this.lights[x-1][y] = !this.lights[x-1][y];
+			this.lights[x][y-1] = !this.lights[x][y-1];
+			
+		} else if(x == 0 && y == 0){
+			
+			this.lights[x][y] = !this.lights[x][y];
+			this.lights[x+1][y] = !this.lights[x+1][y];
+			this.lights[x][y+1] = !this.lights[x][y+1];
+			
+		} else if(x == 5 && y == 0){
+			
+			this.lights[x][y] = !this.lights[x][y];
+			this.lights[x-1][y] = !this.lights[x-1][y];
+			this.lights[x][y+1] = !this.lights[x][y+1];
+			
+		} else if(x == 0 && y == 5){
+			
+			this.lights[x][y] = !this.lights[x][y];
+			this.lights[x+1][y] = !this.lights[x+1][y];
+			this.lights[x][y-1] = !this.lights[x][y-1];
+			
+		} else if(x == 5 && y == 5){
+			
+			this.lights[x][y] = !this.lights[x][y];
+			this.lights[x+-1][y] = !this.lights[x+-1][y];
+			this.lights[x][y-1] = !this.lights[x][y-1];
+			
+		}
 		
 	}
 
