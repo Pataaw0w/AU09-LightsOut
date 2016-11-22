f391aba31a06720b03c83077185fc47a62d036e5
pzhanel <pzhanel@10.0.105.49>
2016-11-22 08:25:43 +0100

    Test

--- src/vincentschmutz/LightsOut.java
+++ src/vincentschmutz/LightsOut.java
@@ -1,10 +1,18 @@
 package vincentschmutz;
 
 /**
- * @author pzhanel
- *
+ * @author Patrick Zhanel
+ * @version 2016-11-19
+ * 
+ * Model-Klasse für die benötigten Methoden.
  */
 
 public class LightsOut {
+	
+	public LightsOut(){
+		
+		
+		
+	}
 
 }
--- src/vincentschmutz/View.java
+++ src/vincentschmutz/View.java
@@ -1,10 +1,12 @@
 package vincentschmutz;
 
 /**
- * @author pzhanel
+ * 
  *
  */
 
 public class View {
+	
+	
 
 }
