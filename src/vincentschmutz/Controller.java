591794eac99a5ec4c2188d9fd992243ada7be11b
Vincent <vincentschwartz7@gmail.com>
2016-11-22 21:49:41 +0100

    Update Controller.java
    fertig

--- src/vincentschmutz/Controller.java
+++ src/vincentschmutz/Controller.java
@@ -16,90 +16,91 @@ public class Controller implements ActionListener{
 	 * Constructor for Controller
 	 */
 	public Controller(){
-		this.p = new Panel(this, this.lo);
 		this.lo = new LightsOut();
+		this.p = new Panel(this, this.lo);
 		this.v = new View(this.p, this.lo, this);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(this.p.isJButton1((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton1());
+			this.p.change(this.p.getJButton1());
 		}
 		if(this.p.isJButton2((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton2());
+			this.p.change(this.p.getJButton2());
 		}
 		if(this.p.isJButton3((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton3());
+			this.p.change(this.p.getJButton3());
 		}
 		if(this.p.isJButton4((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton4());
+			this.p.change(this.p.getJButton4());
 		}
 		if(this.p.isJButton5((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton5());
+			this.p.change(this.p.getJButton5());
 		}
 		if(this.p.isJButton6((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton5());
+			this.p.change(this.p.getJButton5());
 		}
 		if(this.p.isJButton7((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton7());
+			this.p.change(this.p.getJButton7());
 		}
 		if(this.p.isJButton8((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton8());
+			this.p.change(this.p.getJButton8());
 		}
 		if(this.p.isJButton9((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton9());
+			this.p.change(this.p.getJButton9());
 		}
 		if(this.p.isJButton10((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton10());
+			this.p.change(this.p.getJButton10());
 		}
 		if(this.p.isJButton11((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton11());
+			this.p.change(this.p.getJButton11());
 		}
 		if(this.p.isJButton12((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton12());
+			this.p.change(this.p.getJButton12());
 		}
 		if(this.p.isJButton13((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton13());
+			this.p.change(this.p.getJButton13());
 		}
 		if(this.p.isJButton14((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton14());
+			this.p.change(this.p.getJButton14());
 		}
 		if(this.p.isJButton15((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton15());
+			this.p.change(this.p.getJButton15());
 		}
 		if(this.p.isJButton16((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton16());
+			this.p.change(this.p.getJButton16());
 		}
 		if(this.p.isJButton17((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton17());
+			this.p.change(this.p.getJButton17());
 		}
 		if(this.p.isJButton18((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton18());
+			this.p.change(this.p.getJButton18());
 		}
 		if(this.p.isJButton19((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton19());
+			this.p.change(this.p.getJButton19());
 		}
 		if(this.p.isJButton20((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton20());
+			this.p.change(this.p.getJButton20());
 		}
 		if(this.p.isJButton21((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton21());
+			this.p.change(this.p.getJButton21());
 		}
 		if(this.p.isJButton22((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton22());
+			this.p.change(this.p.getJButton22());
 		}
 		if(this.p.isJButton23((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton23());
+			this.p.change(this.p.getJButton23());
 		}
 		if(this.p.isJButton24((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton24());
+			this.p.change(this.p.getJButton24());
 		}
 		if(this.p.isJButton25((JButton)e.getSource())){
-			this.p.hanz(this.p.getJButton25());
+			this.p.change(this.p.getJButton25());
 		}
-		this.p.repaint();
+		this.p.refresh();
 	}
 	
 }
+
 
